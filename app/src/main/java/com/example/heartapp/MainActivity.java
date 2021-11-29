94825ef4feb0442c9761747ba9379fd6379f0ef9
Stefanie Schaller <stefanie.schaller@it-factum.de>
2021-11-29 15:17:32 +0100

    Backup 2.0

--- app/src/main/java/com/example/heartapp/MainActivity.java
+++ app/src/main/java/com/example/heartapp/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.heartapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -21,9 +35,12 @@ import android.widget.Toast;
 
 import com.example.heartapp.visualizer.LineVisualizer;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final int AUDIO_PERMISSION_REQUEST_CODE = 102;
+    public static final int REQUEST_ENABLE_BT = 100;
 
     public static final String[] WRITE_EXTERNAL_STORAGE_PERMS = {
             Manifest.permission.RECORD_AUDIO
@@ -32,15 +49,20 @@ public class MainActivity extends AppCompatActivity {
     protected MediaPlayer mediaPlayer;
 
 
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialize();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
         Spinner modelspinner = (Spinner) findViewById(R.id.modelSpinner);
         Spinner locationspinner = (Spinner) findViewById(R.id.locationSpinner);
-        Spinner speciesspinner = (Spinner) findViewById(R.id.speciesSpinner);
-        RelativeLayout results = findViewById(R.id.resultContainer);
+        Spinner connectionspinner = (Spinner) findViewById(R.id.connectionSpinner);
+        LinearLayout results = findViewById(R.id.resultContainer);
         results.setClipToOutline(true);
         RelativeLayout visualizerContainer = findViewById(R.id.visualizerContainer);
         visualizerContainer.setClipToOutline(true);
@@ -54,20 +76,59 @@ public class MainActivity extends AppCompatActivity {
         locationspinner.setAdapter(locationadapter);
 
         ArrayAdapter<CharSequence> speciesadapter = ArrayAdapter.createFromResource(this,
-                R.array.species, R.layout.dropdown_item);
-        speciesspinner.setAdapter(speciesadapter);
+                R.array.connection, R.layout.dropdown_item);
+        connectionspinner.setAdapter(speciesadapter);
 
 
         LineVisualizer lineVisualizer = findViewById(R.id.visualizer);
         // set custom color to the line.
         lineVisualizer.setColor(ContextCompat.getColor(this, R.color.DarkGreen));
         // set the line with for the visualizer between 1-10 default 1.
-        lineVisualizer.setStrokeWidth(5);
+        lineVisualizer.setStrokeWidth(1);
         // Set you media player to the visualizer.
         lineVisualizer.setPlayer(mediaPlayer.getAudioSessionId());
 
+        int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+
+        //LinearLayout resultGrid = (LinearLayout) findViewById(R.id.resultContainer);
+
+
+        if(width > 2000){
+
+            ViewGroup.MarginLayoutParams params1 = (ViewGroup.MarginLayoutParams) results.getLayoutParams();
+            params1.width = 1500;
+            TextView abnormal = findViewById(R.id.abnormal);
+            abnormal.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
+            TextView normal = findViewById(R.id.normal);
+            normal.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
+            TextView abnormalOutput = findViewById(R.id.abnormalOutput);
+            abnormalOutput.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
+            TextView normalOutput = findViewById(R.id.normalOutput);
+            normalOutput.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
+
+        }
 
     }
+
+
+
+
+
+
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
 
 
 
@@ -87,14 +148,14 @@ public class MainActivity extends AppCompatActivity {
     public void evaluate(View view){
         Spinner modelSpinner = (Spinner) findViewById(R.id.modelSpinner);
         Spinner locationSpinner = (Spinner) findViewById(R.id.locationSpinner);
-        Spinner speciesSpinner = (Spinner) findViewById(R.id.speciesSpinner);
+        Spinner speciesSpinner = (Spinner) findViewById(R.id.connectionSpinner);
         String model = modelSpinner.getSelectedItem().toString();
         String location = locationSpinner.getSelectedItem().toString();
-        String species = speciesSpinner.getSelectedItem().toString();
+        String connection = speciesSpinner.getSelectedItem().toString();
         TextView abnormalOutput = findViewById(R.id.abnormalOutput);
-        abnormalOutput.setText(model + " " + location);
+        abnormalOutput.setText("64 %");
         TextView normalOutput = (TextView) findViewById(R.id.normalOutput);
-        normalOutput.setText(species);
+        normalOutput.setText("36 %");
         Toast.makeText(MainActivity.this, "Evaluating", Toast.LENGTH_LONG).show();
     }
 
@@ -154,4 +215,58 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void connectToBluetoothDevice(){
+
+        // Use this check to determine whether Bluetooth classic is supported on the device.
+// Then you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+// Use this check to determine whether BLE is supported on the device. Then
+// you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "BLE not supported", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+        int requestCode = 1;
+        Intent discoverableIntent =
+                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivityForResult(discoverableIntent, requestCode);
+
+
+
+
+
+    }
+
+    private void connectToWifiDevice(){
+        Toast.makeText(this, "BLE not supported", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+
 }
