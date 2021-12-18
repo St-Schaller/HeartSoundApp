1df3ae2adad5c438d54f6abde528084d7f376a83
Stefanie Schaller <stefanie.schaller1@gmx.net>
2021-12-18 18:19:51 +0100

    Add FileChooser + API Call

--- app/src/main/java/com/example/heartapp/MainActivity.java
+++ app/src/main/java/com/example/heartapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.heartapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,16 +14,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.media.MediaPlayer;
+
+import android.net.Uri;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.os.Bundle;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
@@ -33,8 +37,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.request.SimpleMultiPartRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.heartapp.visualizer.LineVisualizer;
+import com.obsez.android.lib.filechooser.ChooserDialog;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.Channel;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,7 +64,13 @@ public class MainActivity extends AppCompatActivity {
     };
 
     protected MediaPlayer mediaPlayer;
-
+    private BluetoothService bluetoothService = null;
+    private WifiP2pManager manager;
+    private Channel channel;
+    private BroadcastReceiver wifiReceiver;
+    private String url = "https://heartsoundsanalysis.herokuapp.com/predict";
+    private String filePath;
+    private LineVisualizer lineVisualizer;
 
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
@@ -56,12 +79,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialize();
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(receiver, filter);
 
-        Spinner modelspinner = (Spinner) findViewById(R.id.modelSpinner);
-        Spinner locationspinner = (Spinner) findViewById(R.id.locationSpinner);
-        Spinner connectionspinner = (Spinner) findViewById(R.id.connectionSpinner);
+        IntentFilter bluetoothFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bluetoothReceiver, bluetoothFilter);
+        //manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        //channel =  manager.initialize(this, getMainLooper(), null);
+        //wifiReceiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
+        IntentFilter wifiFilter = new IntentFilter();
+        wifiFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        wifiFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        wifiFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        wifiFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        Spinner modelspinner = findViewById(R.id.modelSpinner);
+        Spinner extractorspinner = findViewById(R.id.extractorSpinner);
+        Spinner connectionspinner = findViewById(R.id.connectionSpinner);
         LinearLayout results = findViewById(R.id.resultContainer);
         results.setClipToOutline(true);
         RelativeLayout visualizerContainer = findViewById(R.id.visualizerContainer);
@@ -70,17 +102,15 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> modeladapter = ArrayAdapter.createFromResource(this,
                 R.array.model, R.layout.dropdown_item);
         modelspinner.setAdapter(modeladapter);
-
-        ArrayAdapter<CharSequence> locationadapter = ArrayAdapter.createFromResource(this,
-                R.array.location, R.layout.dropdown_item);
-        locationspinner.setAdapter(locationadapter);
-
+        ArrayAdapter<CharSequence> extractoradapter = ArrayAdapter.createFromResource(this,
+                R.array.extractor, R.layout.dropdown_item);
+        extractorspinner.setAdapter(extractoradapter);
         ArrayAdapter<CharSequence> speciesadapter = ArrayAdapter.createFromResource(this,
                 R.array.connection, R.layout.dropdown_item);
         connectionspinner.setAdapter(speciesadapter);
 
 
-        LineVisualizer lineVisualizer = findViewById(R.id.visualizer);
+        lineVisualizer = findViewById(R.id.visualizer);
         // set custom color to the line.
         lineVisualizer.setColor(ContextCompat.getColor(this, R.color.DarkGreen));
         // set the line with for the visualizer between 1-10 default 1.
@@ -90,33 +120,41 @@ public class MainActivity extends AppCompatActivity {
 
         int width = Resources.getSystem().getDisplayMetrics().widthPixels;
 
-        //LinearLayout resultGrid = (LinearLayout) findViewById(R.id.resultContainer);
+        Button btnChooseFile = (Button) findViewById(R.id.btn_choose_file);
 
+        btnChooseFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ChooserDialog(MainActivity.this)
+                        .withFilter(false, false, "wav")
+                        .withResources(R.string.title_choose_file, R.string.title_choose, R.string.dialog_cancel)
+                        .withChosenListener(new ChooserDialog.Result() {
+                            @Override
+                            public void onChoosePath(String path, File pathFile) {
+                                Toast.makeText(MainActivity.this, "FILE: " + path +" selected", Toast.LENGTH_SHORT).show();
+                                filePath = path;
+                                setPlayerWithPath(pathFile);
+                            }
+                        })
+                        .build()
+                        .show();
+            }
+        });
 
-        if(width > 2000){
+        if (width > 2000) {
 
             ViewGroup.MarginLayoutParams params1 = (ViewGroup.MarginLayoutParams) results.getLayoutParams();
             params1.width = 1500;
-            TextView abnormal = findViewById(R.id.abnormal);
-            abnormal.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
-            TextView normal = findViewById(R.id.normal);
-            normal.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
-            TextView abnormalOutput = findViewById(R.id.abnormalOutput);
-            abnormalOutput.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
-            TextView normalOutput = findViewById(R.id.normalOutput);
-            normalOutput.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
+            TextView resulttextview = findViewById(R.id.resultstextview);
+            resulttextview.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
+            TextView resultsOutput = findViewById(R.id.resultOutput);
+            resultsOutput.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
 
         }
 
     }
 
-
-
-
-
-
-
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+    private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
@@ -130,40 +168,75 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
-
-
     private void initialize() {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M
                 && checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(WRITE_EXTERNAL_STORAGE_PERMS, AUDIO_PERMISSION_REQUEST_CODE);
         } else {
-            setPlayer();
+            setPlayerDefault();
         }
     }
 
-    public void record(View view){
+    public void record(View view) {
         Toast.makeText(MainActivity.this, "Recording", Toast.LENGTH_LONG).show();
+        Spinner connectionSpinner = findViewById(R.id.connectionSpinner);
+        String connection = connectionSpinner.getSelectedItem().toString();
+        if (connection == "Bluetooth") {
+            connectToBluetoothDevice();
+        } else {
+            connectToWifiDevice();
+        }
+
     }
 
-    public void evaluate(View view){
-        Spinner modelSpinner = (Spinner) findViewById(R.id.modelSpinner);
-        Spinner locationSpinner = (Spinner) findViewById(R.id.locationSpinner);
-        Spinner speciesSpinner = (Spinner) findViewById(R.id.connectionSpinner);
+    public void evaluate(View view) {
+        Spinner modelSpinner = findViewById(R.id.modelSpinner);
+        Spinner extractorSpinner = findViewById(R.id.extractorSpinner);
+
         String model = modelSpinner.getSelectedItem().toString();
-        String location = locationSpinner.getSelectedItem().toString();
-        String connection = speciesSpinner.getSelectedItem().toString();
-        TextView abnormalOutput = findViewById(R.id.abnormalOutput);
-        abnormalOutput.setText("64 %");
-        TextView normalOutput = (TextView) findViewById(R.id.normalOutput);
-        normalOutput.setText("36 %");
+        String extractor = extractorSpinner.getSelectedItem().toString();
+        TextView resultOutput = findViewById(R.id.resultOutput);
+
+        SimpleMultiPartRequest multiPartRequestWithParams = new SimpleMultiPartRequest(Request.Method.POST, url,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String data = jsonObject.getString("result");
+                        if(data.equals("1")){
+                            resultOutput.setText("Abnormal");
+                        }else if(data.equals("-1")){
+                            resultOutput.setText("Normal");
+                        }else{
+                            resultOutput.setText("Try again");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Toast.makeText(MainActivity.this, "ConnectionError", Toast.LENGTH_SHORT).show()){
+        };
+        // Add the file here
+        multiPartRequestWithParams.addFile("audiodata",filePath);
+
+        // Add the params here
+        multiPartRequestWithParams.addStringParam("extractor", extractor);
+        multiPartRequestWithParams.addStringParam("model", model);
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        queue.add(multiPartRequestWithParams);
         Toast.makeText(MainActivity.this, "Evaluating", Toast.LENGTH_LONG).show();
     }
 
 
-
-    private void setPlayer() {
+    private void setPlayerDefault() {
         mediaPlayer = MediaPlayer.create(this, R.raw.red_e);
         mediaPlayer.setLooping(false);
+    }
+
+    private void setPlayerWithPath(File file) {
+        mediaPlayer = MediaPlayer.create(this, R.raw.murmur_201101180902);
+        mediaPlayer.setLooping(false);
+        lineVisualizer.setPlayer(mediaPlayer.getAudioSessionId());
     }
 
     public void replay(View view) {
@@ -183,6 +256,7 @@ public class MainActivity extends AppCompatActivity {
                         R.drawable.ic_play_red_48dp));
             } else {
                 mediaPlayer.start();
+                Toast.makeText(this, mediaPlayer.getTrackInfo().toString(), Toast.LENGTH_SHORT).show();
                 btnPlayPause.setImageDrawable(ContextCompat.getDrawable(
                         this,
                         R.drawable.ic_pause_red_48dp));
@@ -209,23 +283,23 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == AUDIO_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                setPlayer();
+                setPlayerDefault();
             } else {
                 this.finish();
             }
         }
     }
 
-    private void connectToBluetoothDevice(){
+    private void connectToBluetoothDevice() {
 
         // Use this check to determine whether Bluetooth classic is supported on the device.
-// Then you can selectively disable BLE-related features.
+        // Then you can selectively disable BLE-related features.
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
             Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
             finish();
         }
-// Use this check to determine whether BLE is supported on the device. Then
-// you can selectively disable BLE-related features.
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, "BLE not supported", Toast.LENGTH_SHORT).show();
             finish();
@@ -237,6 +311,8 @@ public class MainActivity extends AppCompatActivity {
         if (!bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else if (bluetoothService == null) {
+            bluetoothService = new BluetoothService(MainActivity.this, mHandler);
         }
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
         if (pairedDevices.size() > 0) {
@@ -249,24 +325,50 @@ public class MainActivity extends AppCompatActivity {
         int requestCode = 1;
         Intent discoverableIntent =
                 new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        //discoverable for 5 minutes, default 2 minutes
         discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
         startActivityForResult(discoverableIntent, requestCode);
-
-
-
-
-
     }
 
-    private void connectToWifiDevice(){
-        Toast.makeText(this, "BLE not supported", Toast.LENGTH_SHORT).show();
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Toast.makeText(MainActivity.this, "Implement Handler", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private void connectToWifiDevice() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        /*manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                ;
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                ;
+            }
+        });*/
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         //unregister the ACTION_FOUND receiver.
-        unregisterReceiver(receiver);
+        unregisterReceiver(bluetoothReceiver);
+        if(bluetoothService != null){
+            bluetoothService.stop();
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/heartapp/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,44 @@
+package com.example.heartapp;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+
+import java.nio.channels.Channel;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+
+    private WifiP2pManager manager;
+    private Channel channel;
+    private Activity activity;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel,
+                                       Activity activity) {
+        super();
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                // Wifi P2P is enabled
+            } else {
+                // Wi-Fi P2P is not enabled
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+}
